4aaff38583905630646e625ac9270a58dd1ff646
tufusa <tufusa74@gmail.com>
2023-12-27 01:49:21 +0900

    chore: ルーティング

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,16 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { Layout } from 'routes/layout';
 import { Home } from 'pages/home';
+import { UnderConstruction } from 'pages/underConstruction';
 
 export const router = createBrowserRouter([
   {
     path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/',
     element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <Home />
-      }
-    ]
+    children: [{ path: '/1F6A7', element: <UnderConstruction /> }],
   },
 ]);
