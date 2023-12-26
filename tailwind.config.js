342c0242b88140b4fc374b112182936b041d63ba
tufusa <tufusa74@gmail.com>
2023-12-27 01:47:30 +0900

    chore: フォントの設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@
 export default {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: '#FF67A5',
+        back: '#121222',
+      },
+      fontFamily: {
+        sans: [
+          'Noto Sans JP',
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'Segoe UI',
+          'Roboto',
+          'Helvetica Neue',
+          'Arial',
+          'Noto Sans',
+          'sans-serif',
+          'Apple Color Emoji',
+          'Segoe UI Emoji',
+          'Segoe UI Symbol',
+          'Noto Color Emoji',
+        ],
+        'poiret-one': ['Poiret One', 'sans-serif'],
+        urbanist: ['Urbanist', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
